691e433701cd021364073e969f32bafceb8e0764
Roman Martynoff <nonsensr@gmail.com>
2025-01-23 09:59:20 +0300

    Complete exercise 3.8*

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ const PORT = 4444;
 // Enable JSON parser
 app.use(express.json());
 // Enable logging
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :body"));
 
 //
 // Endpoints
