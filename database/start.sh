7a84a830c0f286db892571221f8d21d3dd02560a
Roman Martynoff <nonsensr@gmail.com>
2025-01-26 14:59:31 +0300

    Delete json-server database

--- database/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JSON_SERVER_PORT=${PHONEBOOK_DATABASE_PORT:-3000}
-JSON_SERVER_HOST=${PHONEBOOK_DATABASE_HOST:-localhost}
-
-npx json-server \
-  --watch ./db.json \
-  --host $JSON_SERVER_HOST \
-  --port $JSON_SERVER_PORT
